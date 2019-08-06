0733f0e3ecfded39bd55b63611c344722533fab8
a.petrov <a.petrov@sis-it.pro>
2019-08-06 15:27:03 +0700

    fix ip recognize

--- server/src/main/java/jukebox/core/IpHandshakeInterceptor.java
+++ server/src/main/java/jukebox/core/IpHandshakeInterceptor.java
@@ -1,29 +1,33 @@
 package jukebox.core;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
-import java.util.Map;
-
 public class IpHandshakeInterceptor implements HandshakeInterceptor {
 
     public boolean beforeHandshake(
-            ServerHttpRequest request,
-            ServerHttpResponse response,
-            WebSocketHandler wsHandler,
-            Map<String, Object> attributes
+        ServerHttpRequest request,
+        ServerHttpResponse response,
+        WebSocketHandler wsHandler,
+        Map<String, Object> attributes
     ) {
-        attributes.put("ip", request.getRemoteAddress());
+        List<String> value = request.getHeaders().get("X-Real-IP");
+        if (value == null || value.isEmpty()) {
+            throw new RuntimeException("Empty X-Real-IP");
+        }
+        attributes.put("ip", value.get(0));
         return true;
     }
 
     public void afterHandshake(
-            ServerHttpRequest request,
-            ServerHttpResponse response,
-            WebSocketHandler wsHandler,
-            Exception exception
+        ServerHttpRequest request,
+        ServerHttpResponse response,
+        WebSocketHandler wsHandler,
+        Exception exception
     ) {
     }
 }
