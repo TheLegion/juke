f64b85e49c0aae4cbd6e272e1298720806f941ed
a.petrov <a.petrov@sis-it.pro>
2019-11-30 13:53:48 +0700

    update dependencies

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,50 +1,53 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatSliderModule } from '@angular/material';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InjectableRxStompConfig, RxStompService, rxStompServiceFactory } from '@stomp/ng2-stompjs';
 
-import {AppComponent} from './app.component';
-import {InjectableRxStompConfig, RxStompService, rxStompServiceFactory} from '@stomp/ng2-stompjs';
-import {TrackComponent} from './track/track.component';
-import {TrackControlComponent} from './track-control/track-control.component';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatSliderModule} from '@angular/material';
-import {DurationPipe} from './duration.pipe';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {ScrollingModule} from '@angular/cdk/scrolling';
+import { AppComponent } from './app.component';
+import { DurationPipe } from './duration.pipe';
+import { TrackControlComponent } from './track-control/track-control.component';
+import { TrackComponent } from './track/track.component';
+
+import 'hammerjs';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        TrackComponent,
-        TrackControlComponent,
-        DurationPipe
-    ],
-    imports: [
-        BrowserModule,
-        NoopAnimationsModule,
-        MatCardModule,
-        MatSliderModule,
-        MatButtonModule,
-        MatIconModule,
-        MatInputModule,
-        DragDropModule,
-        ScrollingModule
-    ],
-    providers: [
-        {
-            provide: InjectableRxStompConfig,
-            useFactory: () => ({
-                brokerURL: `ws://${location.host}/api`,
-                reconnectDelay: 500,
-                debug: () => void 0
-            } as InjectableRxStompConfig)
-        },
-        {
-            provide: RxStompService,
-            useFactory: rxStompServiceFactory,
-            deps: [InjectableRxStompConfig]
-        }
-    ],
-    bootstrap: [AppComponent]
-})
+            declarations: [
+              AppComponent,
+              TrackComponent,
+              TrackControlComponent,
+              DurationPipe,
+            ],
+            imports: [
+              BrowserModule,
+              BrowserAnimationsModule,
+              MatCardModule,
+              MatSliderModule,
+              MatButtonModule,
+              MatIconModule,
+              MatInputModule,
+              DragDropModule,
+              ScrollingModule,
+              HammerModule,
+            ],
+            providers: [
+              {
+                provide: InjectableRxStompConfig,
+                useFactory: () => ({
+                  brokerURL: `ws://${location.host}/api`,
+                  reconnectDelay: 500,
+                  debug: () => void 0,
+                } as InjectableRxStompConfig),
+              },
+              {
+                provide: RxStompService,
+                useFactory: rxStompServiceFactory,
+                deps: [InjectableRxStompConfig],
+              },
+            ],
+            bootstrap: [AppComponent],
+          })
 export class AppModule {
 }
