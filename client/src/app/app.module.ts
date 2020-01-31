2fe70f8b9d46e27fc478be2c02ca5d657aa450b0
a.petrov <priboi08@gmail.com>
2020-01-31 19:01:30 +0700

    update deps

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatSliderModule } from '@angular/material';
-import { BrowserModule, HammerModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InjectableRxStompConfig, RxStompService, rxStompServiceFactory } from '@stomp/ng2-stompjs';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {NgModule} from '@angular/core';
+import {BrowserModule, HammerModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {InjectableRxStompConfig, RxStompService, rxStompServiceFactory} from '@stomp/ng2-stompjs';
 
-import { AppComponent } from './app.component';
-import { DurationPipe } from './duration.pipe';
-import { TrackControlComponent } from './track-control/track-control.component';
-import { TrackComponent } from './track/track.component';
+import {AppComponent} from './app.component';
+import {DurationPipe} from './duration.pipe';
+import {TrackControlComponent} from './track-control/track-control.component';
+import {TrackComponent} from './track/track.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
             declarations: [
